d07b5c3bdf989bc495d52b1d3bac91c876fb3601
XavBell <xavier.belleville38@gmail.com>
2022-07-22 19:32:47 -0400

    serializable

--- Assets/saveClasses/saveTank.cs
+++ Assets/saveClasses/saveTank.cs
@@ -1,21 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[System.Serializable]
 public class saveTank
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public string path;
     public string name;
     public float tankSizeX;
