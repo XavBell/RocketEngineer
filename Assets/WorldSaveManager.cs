ae267664a5d02a50b52606b1bac75c582ea87870
XavBell <56218769+XavBell@users.noreply.github.com>
2022-07-31 18:20:51 -0400

    StartedPersistenceSave

--- Assets/WorldSaveManager.cs
+++ Assets/WorldSaveManager.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 [System.Serializable]
 public class WorldSaveManager : MonoBehaviour
 {
+    void FixedUpdate()
+    {
+        saveWorld();
+    }
+
+
     public void saveWorld()
     {
-        public GameObject[] rockets = GameObject.FindGameObjectsWithTag<"capsule">();
+        saveWorld saveWorld = new saveWorld();
+        public GameObject[] rockets = GameObject.FindGameObjectsWithTag("capsule");
+        i = 0;
         foreach(GameObject rocket in rockets)
         {
-            saveWorld saveWorld = new saveWorld();
-            saveWorld.types = new List<string>();
+            if(rocket.GetComponent<Part>().attachBottom.GetComponent<AttachPointScript>().attachedBody != null)
+            {
+                GameObject referenceBody = rocket;
+                while(referenceBody.GetComponent<Part>().attachBottom.GetComponent<AttachPointScript>().attachedBody != null)
+                {
+                    saveWorld.types.Add(referenceBody.GetComponent<Part>().attachBottom.GetComponent<AttachPointScript>().attachedBody.GetComponent<Part>().type);
+                    referenceBody = referenceBody.GetComponent<Part>().attachBottom.GetComponent<AttachPointScript>().attachedBody;
+                    saveWorld.childrenNumber[i]++; 
+                }
+            }
+            i++;
         }
     }
+
+    public void loadWorld()
+    {
+        //TODO
+    }
+
 }
--- Assets/saveClasses/saveWorld.cs
+++ Assets/saveClasses/saveWorld.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 [System.Serializable]
 public class saveWorld
 {
-    public List<string> rocketsID = new List<string>();
-    public List<string> types;
+    public List<string> rockets = new List<string>();
+    public List<int> childrenNumber = new List<int>();
+    public List<string> types = new List<string>();
 }
