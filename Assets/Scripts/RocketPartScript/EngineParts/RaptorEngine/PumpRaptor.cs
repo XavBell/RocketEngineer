5d75e0d404f868e65ec1ee2ca23896879b2569d5
XavBell <56218769+XavBell@users.noreply.github.com>
2023-12-04 00:49:40 -0500

    Update PumpRaptor.cs

--- Assets/Scripts/RocketPartScript/EngineParts/RaptorEngine/PumpRaptor.cs
+++ Assets/Scripts/RocketPartScript/EngineParts/RaptorEngine/PumpRaptor.cs
@@ -6,5 +6,5 @@ public class PumpRaptor : MonoBehaviour
 {
     public string pumpName = "Pump Raptor";
     public float thrust = 10600f;
-    public float mass = 600f;
+    public float mass = 200f;
 }
