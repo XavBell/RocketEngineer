15489f3b585133a16257269f2a5bb65f97ff8c7f
XavBell <56218769+XavBell@users.noreply.github.com>
2023-12-13 06:38:48 -0500

    update

---
+++ Assets/Scripts/RocketPartScript/EngineParts/SmallEngine/TurbineSmall.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurbineSmall : MonoBehaviour
+{
+    public string turbineName = "Turbine Small";
+    public float thrustModifier = 1.5f;
+    public float rate = 50f;
+    public float mass = 500f;
+}
