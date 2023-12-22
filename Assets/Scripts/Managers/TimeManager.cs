d08ae3b1ed2d7b3fbffe01f7978f845ebeba75e5
XavBell <56218769+XavBell@users.noreply.github.com>
2023-12-22 15:35:52 -0500

    fixed deltaTime

--- Assets/Scripts/Managers/TimeManager.cs
+++ Assets/Scripts/Managers/TimeManager.cs
@@ -9,7 +9,6 @@ public class TimeManager : MonoBehaviour
 
     public float scaler = 1;
     public double time = 0;
-    public double normalTime = 0;
     public float deltaTime = 0;
     public float normalDeltaTime;
 
@@ -19,28 +18,19 @@ public class TimeManager : MonoBehaviour
     void Start()
     {
         calculateDeltaTime();
-        calculateNormalDeltaTime();
         time = Time.time;
-        normalTime = Time.time;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         calculateDeltaTime();
-        calculateNormalDeltaTime();
-        normalTime += normalDeltaTime;
         time += deltaTime;
     }
 
     void calculateDeltaTime()
     {
         deltaTime = Time.fixedDeltaTime * scaler;
-    }
-
-    void calculateNormalDeltaTime()
-    {
-        deltaTime = Time.deltaTime;
     }
 
     public void setScaler(float desiredScaler)
