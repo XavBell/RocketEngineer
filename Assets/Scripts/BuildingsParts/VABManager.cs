82afaf7739c1214b1c5cb2103e89c9d1b422f3c0
XavBell <56218769+XavBell@users.noreply.github.com>
2024-06-21 15:25:46 -0400

    Cost is now calculated

--- Assets/Scripts/BuildingsParts/VABManager.cs
+++ Assets/Scripts/BuildingsParts/VABManager.cs
@@ -24,6 +24,8 @@ public class VABManager : MonoBehaviour
 
     public List<string> tanks = new List<string>();
     public List<string> engines = new List<string>();
+    public List<PartData> engineData = new List<PartData>();
+    public List<PartData> tankData = new List<PartData>();
     public bool needUpdate = false;
     public int tempBuilt = 0;
 
@@ -159,9 +161,141 @@ public class VABManager : MonoBehaviour
     {
         if (partName.options.Count > 0)
         {
+            RocketData rocketData = new RocketData();
+            var jsonString = JsonConvert.SerializeObject(rocketData);
+            jsonString = File.ReadAllText(Application.persistentDataPath + savePathRef.worldsFolder + '/' + MasterManager.FolderName + savePathRef.rocketFolder + "/" + partName.options[partName.value].text);
+            RocketData loadedRocket = JsonConvert.DeserializeObject<RocketData>(jsonString);
+            engines.Clear();
+            tanks.Clear();
+            engineData.Clear();
+            tankData.Clear();
+            int totalEngineParts = 0;
+            int totalTankParts = 0;
+            float cost = 0;
             
+            if(loadedRocket.rootPart.partType == "engine")
+            {
+                engineData.Add(loadedRocket.rootPart);
+            }
+            if(loadedRocket.rootPart.partType == "tank")
+            {
+                tankData.Add(loadedRocket.rootPart);
+            }
+            checkChildren(loadedRocket.rootPart);
+            foreach(PartData engine in engineData)
+            {
+                engines.Add(engine.fileName);
+                totalEngineParts += 1;
+                saveEngine saveEngine = new saveEngine();
+                var jsonString1 = JsonConvert.SerializeObject(saveEngine);
+                jsonString1 = File.ReadAllText(Application.persistentDataPath + savePathRef.worldsFolder + '/' + MasterManager.FolderName + savePathRef.engineFolder + engine.fileName);
+                saveEngine loadedEngine = JsonConvert.DeserializeObject<saveEngine>(jsonString1);
+                cost += loadedEngine.cost;
+            }
+
+            foreach(PartData tank in tankData)
+            {
+                tanks.Add(tank.fileName);
+                totalTankParts += 1;
+                saveTank saveTank = new saveTank();
+                var jsonString1 = JsonConvert.SerializeObject(saveTank);
+                jsonString1 = File.ReadAllText(Application.persistentDataPath + savePathRef.worldsFolder + '/' + MasterManager.FolderName + savePathRef.tankFolder + tank.fileName);
+                saveTank loadedTank = JsonConvert.DeserializeObject<saveTank>(jsonString1);
+                cost += loadedTank.cost;
+            }
+
+            engineData.Distinct().ToList();
+            tankData.Distinct().ToList();
+
+            int engineBuilt = 0;
+            int tankBuilt = 0;
+
+            List<string> tempEngines = new List<string>();
+            foreach(string engine in MasterManager.engines)
+            {
+                tempEngines.Add(engine);
+            }
+            int i = 0;
+            foreach(PartData engine in engineData)
+            {
+                if(tempEngines.Contains(engine.fileName.Replace("/", "")))
+                {
+                    engineBuilt++;
+                    tempEngines.RemoveAt(tempEngines.IndexOf(engine.fileName.Replace("/", "")));
+                    saveEngine saveEngine = new saveEngine();
+                    var jsonString1 = JsonConvert.SerializeObject(saveEngine);
+                    jsonString1 = File.ReadAllText(Application.persistentDataPath + savePathRef.worldsFolder + '/' + MasterManager.FolderName + savePathRef.engineFolder + engine.fileName);
+                    saveEngine loadedEngine = JsonConvert.DeserializeObject<saveEngine>(jsonString1);
+                    cost -= loadedEngine.cost;
+
+                }
+            }
+
+            List<string> tempTanks = new List<string>();
+            foreach(string tank in MasterManager.tanks)
+            {
+                tempTanks.Add(tank);
+            }
+            i = 0;
+            foreach(PartData tank in tankData)
+            {
+                if(tempTanks.Contains(tank.fileName.Replace("/", "")))
+                {
+                    tankBuilt++;
+                    tempTanks.RemoveAt(tempTanks.IndexOf(tank.fileName.Replace("/", "")));
+                    saveTank saveTank = new saveTank();
+                    var jsonString1 = JsonConvert.SerializeObject(saveTank);
+                    jsonString1 = File.ReadAllText(Application.persistentDataPath + savePathRef.worldsFolder + '/' + MasterManager.FolderName + savePathRef.tankFolder + tank.fileName);
+                    saveTank loadedTank = JsonConvert.DeserializeObject<saveTank>(jsonString1);
+                    cost -= loadedTank.cost;
+                }
+            }
+
+            int built = 0;
+            foreach(string rocket in MasterManager.rockets)
+            {
+                if(rocket == partName.options[partName.value].text)
+                {
+                    built += 1;
+                }
+            }
+
+            tempBuilt = built;
+
+            engineBuiltTxt.gameObject.SetActive(true);
+            tankBuiltTxt.gameObject.SetActive(true);
+            tankText.SetActive(true);
+            engineText.SetActive(true);
+            builtTxt.text = built.ToString();
+
+            costTxt.text = cost.ToString();
+            engineBuiltTxt.text = engineBuilt.ToString() + "/" + totalEngineParts.ToString();
+            tankBuiltTxt.text = tankBuilt.ToString() + "/" + totalTankParts.ToString();
+
+
         }
 
+    }
+
+    public void checkChildren(PartData parent)
+    {
+        foreach (PartData part in parent.children)
+        {
+            if(part.partType == "engine")
+            {
+                engineData.Add(part);
+            }
+
+            if(part.partType == "tank")
+            {
+                tankData.Add(part);
+            }
+
+            if (part.children.Count > 0)
+            {
+                checkChildren(part);
+            }
+        }
     }
 
     void OnTankSelected()
