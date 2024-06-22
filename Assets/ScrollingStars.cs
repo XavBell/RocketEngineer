25be774420614f2cd021af9f5f506528ec69ab10
rtificr <hummusman5@gmail.com>
2024-06-22 13:36:54 -0400

    Separated menu and game background

--- Assets/ScrollingStars.cs
+++ Assets/ScrollingStars.cs
@@ -10,7 +10,7 @@ public class ScrollingStars : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        scroll = Vector3.zero;
     }
 
     // Update is called once per frame
