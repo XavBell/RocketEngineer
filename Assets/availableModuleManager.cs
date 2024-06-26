c1058c714edeb1de543249f6a3015bd64e229a16
XavBell <56218769+XavBell@users.noreply.github.com>
2024-06-26 10:24:41 -0400

    Imported new textures

--- Assets/availableModuleManager.cs
+++ Assets/availableModuleManager.cs
@@ -2,14 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CapsuleComponent : MonoBehaviour
+public class availableModuleManager : MonoBehaviour
 {
-    public Sprite interiorSprite;
-    public Sprite exteriorSprite;
-    public GameObject internalEditor;
-    public GameObject externalEditor;
-
-    public List<CapsuleModuleComponent> modules;
+    public List<GameObject> availableModules;
     // Start is called before the first frame update
     void Start()
     {
